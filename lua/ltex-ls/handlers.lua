92c4295ba7e128cec852435af9db50d2954369d1
Thomas Vigouroux <thomas.vigouroux@protonmail.com>
2022-06-13 09:52:29 +0200

    feat: allow to add things into external files

--- lua/ltex-ls/handlers.lua
+++ lua/ltex-ls/handlers.lua
@@ -2,13 +2,60 @@ local M = {}
 
 local cache = require 'ltex-ls.cache'
 local externals = require 'ltex-ls.externals'
+local utils = require 'ltex-ls.utils'
 
-local function handle_option_update(client, updated_val, uri)
-  local ltex_settings = client.config.settings.ltex
+local function handle_option_update(client, key, updated_val, uri)
+  -- FIXME(vigoux): this does a lot of little write operations on the cache file, maybe have
+  --                something to delay cache write ?
+  if not utils.check_config_key(key) then return end
 
+  local ltex_settings = client.config.settings.ltex[key]
   local fpath = vim.uri_to_fname(uri)
-  cache.update_cache(fpath, updated_val)
 
+  if not ltex_settings then
+    -- Just push to the cache
+    cache.update_cache(fpath, { [key] = updated_val })
+    return
+  end
+
+  for lang, value in pairs(updated_val) do
+    if ltex_settings[lang] then
+      -- There are defined configuration for this language, not just the cache
+      -- so check if there is not an external file where the user might want to append
+      local external_files = vim.tbl_map(function(e) return e:sub(2) end, externals.filter(ltex_settings[lang]))
+
+      if #external_files > 0 then
+
+        -- User configured an external file, ask where to put the newly added thing
+
+        table.insert(external_files, "Local cache file")
+        vim.ui.select(external_files, {
+          prompt = string.format('Where to store new "%s":', key),
+          kind = "string"
+        }, function(item, index)
+          if not index then
+            return
+          elseif index == #external_files then
+            -- The user asked to write into the cache
+            cache.update_cache(fpath, { [key] = { [lang] = value } })
+          else
+            local destfile = io.open(item, "a")
+            if destfile then
+              destfile:write(table.concat(value, "\n"))
+              destfile:close()
+            else
+              utils.log("Can't write to " .. item, vim.log.levels.ERROR)
+            end
+          end
+        end)
+
+      else
+        cache.update_cache(fpath, { [key] = { [lang] = value } })
+      end
+    else
+      cache.update_cache(fpath, { [key] = { [lang] = value } })
+    end
+  end
   client.checkDocument(uri)
 end
 
@@ -19,13 +66,13 @@ function M.workspace_command(err, result, ctx, config)
   end
 
   local command_name = ctx.params.command
-  local args = ctx.params.arguments
+  local arg = ctx.params.arguments[1]
   if command_name == "_ltex.addToDictionary" then
-    handle_option_update(client, { dictionnary = args[1].words }, args[1].uri)
+    handle_option_update(client, "dictionary",  arg.words, arg.uri)
   elseif command_name == "_ltex.hideFalsePositives" then
-    handle_option_update(client, { hiddenFalsePositives = args[1].falsePositives }, args[1].uri)
+    handle_option_update(client, "hiddenFalsePositives", arg.falsePositives, arg.uri)
   elseif command_name == "_ltex.disableRules" then
-    handle_option_update(client, { disabledRules = args[1].ruleIds }, args[1].uri)
+    handle_option_update(client, "disabledRules", arg.ruleIds, arg.uri)
   end
 
   vim.lsp.handlers[ctx.method](err, result, ctx, config)
@@ -39,6 +86,7 @@ function M.workspace_configuration(err, result, ctx, config)
 
   local expanded = externals.expand_config(settings)
   expanded = cache.merge_with(vim.uri_to_fname(scope_uri), expanded)
+  vim.pretty_print(expanded)
   return expanded
 end
 
--- lua/ltex-ls/utils.lua
+++ lua/ltex-ls/utils.lua
@@ -11,4 +11,16 @@ function M.read_dictionnary_file(path)
   return lines
 end
 
+function M.log(msg, level, opts)
+  vim.notify(string.format("[lext-ls.nvim]: %s", msg), level, opts or {})
+end
+
+function M.check_config_key(key)
+  if not vim.tbl_contains(M.CONFIG_KEYS, key) then
+    M.log(key .. " is not a configurable entry", vim.log.levels.ERROR)
+    return false
+  end
+  return true
+end
+
 return M
